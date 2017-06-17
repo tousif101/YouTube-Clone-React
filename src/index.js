0df455b1e5dcadb8bfd777dc45bba38774caea36
Tousif Chowdhury <tac5780@rit.edu>
2017-06-17 15:08:13 -0400

    Updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_details';
+
+//Class based components vs functional components
 
 //1) Create a new component. Component produces some html 
 //2) Make sure the component gets put onto the DOM. 
@@ -40,9 +42,12 @@ class App extends Component {
 
     //Videos is null at first when it loads, then gets information. Cant fetch info fast enough for children to use.
     render(){
+        //Throttle the search term. Debounce it. Wait 300 seconds after you type it out 
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
         return (
             <div>
-                <SearchBar onSearchTermChange = {term => this.videoSearch(term)}/>
+                <SearchBar onSearchTermChange = {videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                     onVideoSelect = {selectedVideo => this.setState({selectedVideo}) }
